9a6f6276b34c08e0ce591f7b1d112c980e1929f8
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2019-10-07 00:59:51 +0300

    pdb example

--- generate.js
+++ generate.js
@@ -4,42 +4,93 @@ const nano = require("nano")(url);
 const db = nano.db.use("couch");
 
 let newPerson = {
-    from: 1,
-    to: 10,
+  from: 1,
+  to: 10,
 
-    [Symbol.asyncIterator]() {
+  [Symbol.asyncIterator]() {
+    return {
+      current: this.from,
+      last: this.to,
 
-        return {
-            current: this.from,
-            last: this.to,
+      async next() {
+        const res = await db.insert({
+          name: faker.fake("{{name.firstName}} {{name.lastName}}"),
+          email: faker.internet.email(),
+          country: faker.address.country(),
+          phone: faker.phone.phoneNumber(),
+          date: faker.date.past(2),
+          level: faker.random.arrayElement([
+            "Beginner",
+            "Elementary",
+            "Intermediate",
+            "Upper-Intermediate",
+            "Advanced",
+            "Proficiency"
+          ])
+        });
 
-            async next() {
+        console.log(res);
 
-                const res = await db.insert({
-                    name: faker.fake("{{name.firstName}} {{name.lastName}}"),
-                    email: faker.internet.email(),
-                    country: faker.address.country(),
-                    phone: faker.phone.phoneNumber(),
-                    date: faker.date.past(2),
-                    "level": faker.random.arrayElement(["Beginner", "Elementary", "Intermediate", "Upper-Intermediate", "Advanced", "Proficiency"])
-                });
-
-                console.log(res);
-
-                if (this.current <= this.last) {
-                    return { done: false, value: this.current++ };
-                } else {
-                    return { done: true };
-                }
-            }
-        };
-    }
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
 };
 
 (async () => {
+  for await (let value of newPerson) {
+    console.log(value);
+  }
+})();
 
-    for await (let value of newPerson) {
-        console.log(value);
-    }
 
-})()
+//Заполнить бд тестовыми данными
+var cdb = new PouchDB("test");
+
+var gdb = {
+  from: 0,
+  to: games.length - 1,
+
+  [Symbol.asyncIterator]() {
+    return {
+      current: this.from,
+      last: this.to,
+
+      async next() {
+        debugger;
+        const fileName = games[this.current].slug;
+        const blobData = await fetch(`data/img/${fileName}.jpg`).then(res => {
+          return res.blob();
+        });
+
+        const res = await cdb.post({
+          ...games[this.current],
+          _attachments: {
+            [fileName]: {
+              content_type: blobData.type,
+              data: blobData
+            }
+          }
+        });
+
+        console.log(res);
+
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+};
+
+(async () => {
+  for await (let value of gdb) {
+    console.log(value);
+  }
+})();
