2bf5819c10c08ac091abbe050e5aeef502d29e2a
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2019-10-31 21:46:51 +0300

    add document to backend

--- public/main.js
+++ public/main.js
@@ -8,6 +8,17 @@
       window.location.assign("/login");
     }
     document.getElementById("userName").innerText = currentUser.userCtx.name;
+    const db = new PouchDB("http://localhost:5984/test");
+    const addForm = document.getElementById("addForm");
+    const addButton = document.getElementById("addButton");
+    addButton.addEventListener("click", async e => {
+      try {
+        let result = await db.put({ _id: addForm.text.value, text: "hello" });
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    });
     console.log("current name", currentUser.userCtx.name);
   }
 })();
