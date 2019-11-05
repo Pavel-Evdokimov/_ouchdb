e5ab12ef0fb06fe4c619267a47d44d14b534ab68
Pavel Evdokimov <p_evdokimov@chebit24.ru>
2019-11-05 21:28:24 +0300

    render new item implemented

---
+++ examples/updateFunctionForTest.js
@@ -0,0 +1,27 @@
+const PouchDB = require("pouchdb-core");
+PouchDB.plugin(require("pouchdb-adapter-http"));
+PouchDB.plugin(require("pouchdb-mapreduce"));
+
+const url = "http://localhost:5984/";
+const db = new PouchDB(`${url}test`, {
+  auth: { username: "admin", password: "admin" }
+});
+
+const designDoc = {
+  _id: "_design/app",
+  updates: {
+    test: function(doc, req) {
+      var body = JSON.parse(req.body);
+      body["test"] = "this is test";
+      return [body, "ok"];
+    }.toString()
+  }
+};
+
+db.put(designDoc)
+  .then(val => {
+    console.log(val);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- public/main.js
+++ public/main.js
@@ -1,4 +1,23 @@
 (async () => {
+  const db = new PouchDB("http://localhost:5984/test");
+
+  const addContent = async docId => {
+    let content = document.getElementById("content");
+    let article = document.createElement("article");
+
+    let { _id, _attachments, ...doc } = await db.get(docId);
+    let imageName = Object.getOwnPropertyNames(_attachments)[0];
+
+    article.setAttribute("id", _id);
+
+    let template = `
+    <img src="http://localhost:5984/test/${_id}/${imageName}">
+    <h3>${doc.title}</h3>
+    <p>${doc.text}</p>
+    `;
+    article.innerHTML = template;
+    content.appendChild(article);
+  };
   //get current rout
   if (window.location.pathname === "/") {
     let currentUser = await fetch("http://localhost:5984/_session", {
@@ -8,15 +27,15 @@
       window.location.assign("/login");
     }
     document.getElementById("userName").innerText = currentUser.userCtx.name;
-    const db = new PouchDB("http://localhost:5984/test");
     const addForm = document.getElementById("addForm");
     const addButton = document.getElementById("addButton");
     addButton.addEventListener("click", async e => {
       const photo = addForm.photo.files[0];
       try {
-        let result = await db.put({
-          _id: addForm.text.value,
-          text: "hello",
+        let result = await db.post({
+          title: addForm.title.value,
+          text: addForm.text.value,
+          userName: currentUser.userCtx.name,
           _attachments: {
             [photo.name]: {
               content_type: photo.type,
@@ -24,12 +43,14 @@
             }
           }
         });
+        addContent(result.id);
         console.log(result);
       } catch (error) {
         console.log(error);
       }
     });
-    console.log("current name", currentUser.userCtx.name);
+    // Получить все записи пользователя и отрисовать их
+    console.log("current name", currentUser);
     setTimeout(() => {
       // error
       const a = b * b;
